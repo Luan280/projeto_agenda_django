20c3b2eb79a5911c5d1232f7e5f3136ff6e02d51
LuanFerreiraLima <luanferreiralima280@gmail.com>
2025-01-21 22:11:36 -0300

    Model Category, campo owner e Model Meta usando o Model User

--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    # Seta como deve ser o nome da categoria tanto no plural como no singular
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Contact(models.Model):
     firts_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True) # blank torna esse campo opcional
@@ -12,7 +25,19 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%y/%m/') # Dentro da rota de media dentro da pasta "pictures" vai ter uma pasta com ano e mês de criação
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
+    # "on_delete" Se eu deletar a categoria, o que eu faço com o contato
 
-
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
     def __str__(self):
         return f"{self.firts_name} {self.last_name}"
