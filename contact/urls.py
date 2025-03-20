c846e3d5aa892b185c8b6a6586723e172a15b4d6
LuanFerreiraLima <luanferreiralima280@gmail.com>
2025-03-20 18:07:59 -0300

    Contact.html, /partials, include e header.html

--- contact/urls.py
+++ contact/urls.py
@@ -4,5 +4,6 @@ from contact import views
 app_name = "contact" # NAME_SPACE
 
 urlpatterns = [
+    path('<int:contact_id>/', views.contact, name="contact"),
     path('', views.index, name="index"),
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
+
 
 # Create your views here.
 def index(request):
@@ -7,8 +8,20 @@ def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
     context = {
         'contacts': contacts,
+        "site_title": "Contatos -"
     }
     return render(request,
                   "contact/index.html",
                   context
 )
+ 
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    # Verifica se o retorno da consulta é None, se sim, retorna o erro 404
+    single_contact = get_object_or_404(Contact.objects.filter(pk=contact_id, show=True))
+    site_title = f'{single_contact.firts_name} {single_contact.last_name} -'
+    context = {
+        "contact": single_contact,
+        "site_title": site_title,
+    }
+    return render(request, "contact/contact.html", context)
