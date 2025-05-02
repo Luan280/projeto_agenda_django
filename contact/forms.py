24d47dcd3ba697a639d7af35047b553807abc3c1
LuanFerreiraLima <luanferreiralima280@gmail.com>
2025-05-02 17:38:06 -0300

    Criação e atualização de contatos

---
+++ contact/forms.py
@@ -0,0 +1,69 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from . import models
+
+
+class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe-a',
+                'placeholder': "Escreva aqui"
+            }
+        ),
+        help_text="Texto de ajuda para o seu usuário",
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Atualizando o widget
+        # self.fields['first_name'].widget.attrs.update({
+        #     'class': 'classe-a',
+        #     'placeholder': "Escreva aqui"
+        # })
+
+    class Meta:
+        model = models.Contact
+        fields = (
+            "first_name", "last_name", "phone",
+            "email", "description", "category",
+
+        )
+        # Criando um novo widget para o campo
+        # widgets = {
+        #     "first_name": forms.TextInput(
+        #         attrs={
+        #             'class': 'classe-a',
+        #             'placeholder': "Escreva aqui"
+        #         }
+        #     ),
+        # }
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        print
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+ 
+        if first_name == last_name:
+            msg = ValidationError(
+                "Primeiro nome não pode ser igual o segundo",
+                code="invalid",
+            )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
+        return super().clean()
+
+    def clean_first_name(self,):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == "ABC":
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Não digite ABC neste campo',
+                    code="invalid"
+                ))
+        return first_name
