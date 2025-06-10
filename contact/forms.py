6563779028142d3e0e237f5a338a9a0aead9b785
LuanFerreiraLima <luanferreiralima280@gmail.com>
2025-06-10 00:50:48 -0300

    adicionando o templete de login, register, partials/-user-form. Trabalhando com mensagens de erro e sucesso

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from . import models
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 
 class ContactForm(forms.ModelForm):
@@ -15,12 +18,13 @@ class ContactForm(forms.ModelForm):
     )
 
     picture = forms.ImageField(
-        widget=forms.FileField(
+        widget=forms.FileInput(
             attrs={
                 'accept': 'image/*',
             }
         )
     )
+
     class Meta:
         model = models.Contact
         fields = (
@@ -29,11 +33,9 @@ class ContactForm(forms.ModelForm):
             "picture",
 
         )
-    
 
     def clean(self):
         cleaned_data = self.cleaned_data
-        print
         first_name = cleaned_data.get('first_name')
         last_name = cleaned_data.get('last_name')
 
@@ -58,3 +60,33 @@ class ContactForm(forms.ModelForm):
                     code="invalid"
                 ))
         return first_name
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Já existe este e-mail', code='invalid')
+            )
+
+        return email
--- contact/urls.py
+++ contact/urls.py
@@ -6,12 +6,16 @@ app_name = "contact"  # NAME_SPACE
 urlpatterns = [
     path('search/', views.search, name="search"),
     path('', views.index, name="index"),
+
     # Contact (CRUD)
     path('contact/<int:contact_id>/detail/', views.contact, name="contact"),
     path('contact/create/', views.create, name="create"),
     path('contact/<int:contact_id>/update/', views.update, name="update"),
     path('contact/<int:contact_id>/delete/', views.delete, name="delete"),
 
-    
+    # user
+    path('user/create/', views.register, name="register"),
+    path('user/login/', views.login_view, name="login"),
+    path('user/logout/', views.logout_view, name="logout"),
 
 ]
