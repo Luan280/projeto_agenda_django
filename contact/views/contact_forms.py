89ccede48a6ed12c42fcaab5f88f942a6f5c9612
LuanFerreiraLima <luanferreiralima280@gmail.com>
2025-06-11 23:55:06 -0300

    adicionando ultimo detalhes antes do deploy, sistema de login e logout, user_authenticated para diferenciar usuáros logados, garatindo usuário está logado para acessar as view e relação entre contato e owner

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from contact.forms import ContactForm
 from contact.models import Contact
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
 
@@ -16,7 +19,10 @@ def create(request):
         }
 
         if form.is_valid():
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
+
             return redirect('contact:update', contact_id=contact.pk)
 
         return render(request,
@@ -35,11 +41,13 @@ def create(request):
                   )
 
 
+@login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(
         Contact,
         pk=contact_id,
-        show=True
+        show=True,
+        owner=request.user
     )
     form_action = reverse('contact:update', args=(contact_id,))
 
@@ -71,15 +79,17 @@ def update(request, contact_id):
                   )
 
 
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(Contact, pk=contact_id, show=True,
+        owner=request.user)
 
     confirmation = request.POST.get("confirmation", 'no')
 
     if confirmation == "yes":
         contact.delete()
         return redirect('contact:index')
-    
+
     return render(
         request, 'contact/contact.html',
         {
